9c71b462743b9fd2f5411744d5b349356135898c
chouretr <chouretr@gmail.com>
2016-02-01 16:34:40 +0100

    création d'une table recette chaque attribut de la classe recette correspondra a un champ dans la table définitions des setters et getters

---
+++ Model/Class/autoloader.php
@@ -0,0 +1,20 @@
+<?php
+
+/**
+ * Created by PhpStorm.
+ * User: Robin
+ * Date: 27/01/2016
+ * Time: 16:02
+ */
+class Autoloader
+{
+    static function register()
+    {
+        spl_autoload_register(array(__CLASS__, 'autoload'));
+    }
+
+    static function autoload($class_name)
+    {
+        require 'Model/Class/' .$class_name. '.php';
+    }
+}
