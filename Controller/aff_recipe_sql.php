03b6f5345744d6d61f5b9af147d7e7a7d71ec308
chouretr <chouretr@gmail.com>
2016-02-18 20:07:38 +0100

    modif de scripts.js et page2 ==> ajout des étapes de préparation de la recette finie

--- Controller/aff_recipe_sql.php
+++ Controller/aff_recipe_sql.php
@@ -1,2 +1,22 @@
 <?php
+include '../Model/Class/Database.php';
+$db = new Database('marmiton');
 
+$_GET['recette'] = "1";
+$sql = ('SELECT categorie.nom
+              FROM categorie, categories_recette
+              Where categories_recette.categorie_id = categorie.id
+              AND categories_recette.recette_id = ?');
+try
+{
+    $data = $db->prepare($sql, array($_GET['recette']));
+}
+catch(Exception $e)
+{
+    ('Erreur : '.$e->getMessage());
+}
+foreach ($db->prepare($sql, array($_GET['recette'])) as $data)
+{
+    echo $data->nom;
+}
+var_dump($data);
--- Controller/ingredient.php
+++ Controller/ingredient.php
@@ -1,28 +1,10 @@
 <?php
-/*$dbHost = 'localhost';
-$dbUsername = 'root';
-$dbPassword = '';
-$dbName = 'marmiton';
-//connect with the database
-$db = new mysqli($dbHost,$dbUsername,$dbPassword,$dbName);
-//get search term*/
 include '../Model/Class/Database.php';
-
 $db = new Database('marmiton');
-
 $searchTerm = $_GET['term'];
-//get matched data from skills table
-
 foreach ($db->query("SELECT * FROM ingredients WHERE nom Like '%".$searchTerm."%' ORDER BY nom ASC") AS $row);
 {
     $data[] = $row->nom;
 }
-
-/*$query = $db->query("SELECT * FROM ingredients WHERE nom LIKE '%".$searchTerm."%' ORDER BY nom ASC");
-while ($row = $query->fetch_assoc()) {
-    $data[] = $row['nom'];
-}*/
-
-//return json data;
 echo json_encode($data);
 ?>
