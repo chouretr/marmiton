910051a345d988b591636f2abd6e6ff224f5a350
Layer-Etna <alary20@hotmail.fr>
2016-02-18 15:30:08 +0100

    Script JS modif multiple champs

--- Pages/page2.php
+++ Pages/page2.php
@@ -104,6 +104,10 @@
                 </div>
                 <div class="col-md-2"></div>
             </div>
+            <div class="ui-widget">
+                <label for="tags">Tags: </label>
+                <input type="text" class="tags">
+            </div>
             <div class="input_fields_wrap">
                 <!-- contenu JS    -->
             </div>
--- View/js/scripts.js
+++ View/js/scripts.js
@@ -5,57 +5,56 @@
 });*/
 
 $(function() {
-    var availableTags = [
-        "ActionScript",
-        "AppleScript",
-        "Asp",
-        "BASIC",
-        "C",
-        "C++",
-        "Clojure",
-        "COBOL",
-        "ColdFusion",
-        "Erlang",
-        "Fortran",
-        "Groovy",
-        "Haskell",
-        "Java",
-        "JavaScript",
-        "Lisp",
-        "Perl",
-        "PHP",
-        "Python",
-        "Ruby",
-        "Scala",
-        "Scheme"
-    ];
-
+    var x = 1;
     run();
 
+    function run() {
+        $(".tags").autocomplete({
+            source: 'Controller/ingredient.php'
 
-run();
+        });
+    }
 
-function run() {
-    $(".tags").autocomplete({
-        source: 'Controller/ingredient.php'
-
+    $("#add_ingredient").on("click", function () {
+        x++;
+        $('<input type="text" class="tags" /><a href="#" class="btn btn-default remove_field"><span class="glyphicon glyphicon-remove"></span></a>').appendTo('.ui-widget');
+        $(".tags").each(function () {
+            run();
+        });
+        //   i++; your code is here
+        return false;
     });
-}
+    var wrapper         = $(".ui-widget");
+    $(wrapper).on("click",".remove_field", function(e)
+    { //user click on remove text
+        e.preventDefault(); $(this).parent('div').remove(); x--;
+    })
+});
 
+/*$(document).ready(function()
+{
+    var max_fields      = 10; //maximum input boxes allowed
+    var wrapper         = $(".ui-widget"); //Fields wrapper
+    var add_button      = $("#add_ingredient"); //Add button ID
 
-$( "#add_ac" ).on( "click", function() {
-    $('<input type="text" class="tags" />').appendTo('.ui-widget');
-    $(".tags").each(function() {
+    var x = 1; // nombre initial
+    $(add_button).click(function(e)
+    { // sur un click "ajouter"
+        e.preventDefault();
+        if(x < max_fields){ //max input box allowed
+            x++; //text box increment
+            $(wrapper).append('<input type="text" class="tags">'
+            );
+        }
         run();
     });
-    //   i++; your code is here
-    return false;
+
+    $(wrapper).on("click",".remove_field", function(e)
+    { //user click on remove text
+        e.preventDefault(); $(this).parent('div').remove(); x--;
+    })
 });
-
-});
-
-
-$(document).ready(function()
+/*$(document).ready(function()
 {
     var max_fields      = 10; //maximum input boxes allowed
     var wrapper         = $(".input_fields_wrap"); //Fields wrapper
@@ -68,20 +67,24 @@ $(document).ready(function()
         if(x < max_fields){ //max input box allowed
             x++; //text box increment
             $(wrapper).append('<div>' +
-                                  '<div class="col-md-4"></div>' +
-                                  '<div class="col-md-2">' +
-                                          '<input id="textinput" name="quantité" type="number" placeholder="0" class="form-control input-md">' +
-                                          '<p class="help-block inline">quantité</p>' +
-                                  '</div>' +
-                                  '<div class="col-md-2">' +
-                                          '<input id="www" type="text" list="urldata" name="adresseweb" class="form-control input-md">' +
-                                          '<p class="help-block inline">unité</p>' +
+                '<div class="col-md-4"></div>' +
+                '<div class="col-md-2">' +
+                '<input id="textinput" name="quantité" type="number" placeholder="0" class="form-control input-md">' +
+                '<p class="help-block inline">quantité</p>' +
+                '</div>' +
+                '<div class="col-md-2">' +
+                '<input id="www" type="text" list="urldata" name="adresseweb" class="form-control input-md">' +
+                '<p class="help-block inline">unité</p>' +
 
-                                  '</div>' +
+                '</div>' +
+                '<div class="ui-widget">' +
+                '<input type="text" class="tags" />' +
+                '<label for="tags">ingrédient</label>' +
+                '</div>' +
                 '<a href="#" class="btn btn-default remove_field"><span class="glyphicon glyphicon-remove"></span></a>' +
-                                  '<div class="col-md-4"></div>' +
+                '<div class="col-md-4"></div>' +
 
-                              '</div>'); // ajout d'une ligne d'inputs pour ajouter un ingrédient
+                '</div>'); // ajout d'une ligne d'inputs pour ajouter un ingrédient
         }
     });
 
@@ -89,4 +92,4 @@ $(document).ready(function()
     { //user click on remove text
         e.preventDefault(); $(this).parent('div').remove(); x--;
     })
-});
+});*/
